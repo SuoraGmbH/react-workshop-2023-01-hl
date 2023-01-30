aa5cd6e4981b8546790fbacb6530e78957a9ef5a
Hans-Christian Otto <c.otto@suora.com>
2023-01-30 13:09:52 +0100

    show react.fragment

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,12 @@ import { createRoot } from "react-dom/client";
 const root = createRoot(document.getElementById("root")!);
 
 function App() {
-  return <h2>Hallo, mein Name ist Sarah!</h2>;
+  return (
+    <>
+      <h1>Hallo Hamburg!</h1>
+      <h2>Hallo, mein Name ist Sarah!</h2>
+    </>
+  );
 }
 
 root.render(<App />);
