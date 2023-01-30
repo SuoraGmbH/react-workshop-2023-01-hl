b38ccf3e83208c388aa5b886cdcc642cc3040104
Hans-Christian Otto <c.otto@suora.com>
2023-01-30 13:12:47 +0100

    move app to separate file

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Hallo Hamburg!</h1>
+      <h2>Hallo, mein Name ist Sarah!</h2>
+    </>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 
 const root = createRoot(document.getElementById("root")!);
-
-function App() {
-  return (
-    <>
-      <h1>Hallo Hamburg!</h1>
-      <h2>Hallo, mein Name ist Sarah!</h2>
-    </>
-  );
-}
 
 root.render(<App />);
