142a8107026a8f0d6e37f6434779d7a32d06bf09
Hans-Christian Otto <c.otto@suora.com>
2023-01-31 09:29:34 +0100

    use State

--- src/components/TimeEntryForm.tsx
+++ src/components/TimeEntryForm.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TimeEntryForm: React.FunctionComponent = () => {
+  const [inputValue, setInputValue] = useState("");
+
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     console.log("Hallo Welt!");
   }
 
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(event.target.value);
+  }
+
   return (
     <form onSubmit={handleSubmit}>
+      <label>
+        Kommentar
+        <input onChange={handleChange} />
+      </label>
+      {inputValue}
       <button type="submit">Speichern</button>
     </form>
   );
