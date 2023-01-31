7bebf2e9c0bf0272432b2f482faaca9997265cea
Hans-Christian Otto <c.otto@suora.com>
2023-01-31 11:22:26 +0100

    fix counter, show useEffect

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { ThemedBox } from "./Theme";
 
 const Counter: React.FunctionComponent = () => {
   const [count, setCount] = useState(0);
 
+  useEffect(
+    function () {
+      document.title = `Aktueller Zähler: ${count}`;
+    },
+    [count]
+  );
+
   return (
     <div>
+      <ThemedBox />
       <p>Current count: {count}</p>
       <button
         onClick={() => {
-          setCount(count + 1);
+          setCount(function (prevCount) {
+            return prevCount + 1;
+          });
         }}
       >
         Increment
