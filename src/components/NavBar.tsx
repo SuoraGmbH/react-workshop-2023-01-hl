c3218dcb54b21687e9c02b82ac60cfe0db5ac204
Hans-Christian Otto <c.otto@suora.com>
2023-02-01 13:51:39 +0100

    use styled components

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { getTimeEntries } from "../redux/timeEntries/timeEntriesReducer";
+import styled from "styled-components";
+
+const NavBarDiv = styled.div<{ primary: boolean }>`
+  background: black;
+  color: ${(props) => {
+    if (props.primary) {
+      return "pink";
+    }
+    return "white";
+  }};
+  text-align: right;
+`;
 
 const NavBar: React.FunctionComponent = () => {
   const timeEntries = useSelector(getTimeEntries);
 
   return (
-    <div
-      style={{
-        background: "black",
-        color: "white",
-        textAlign: "right",
-      }}
-    >
-      {timeEntries.length} Time Entries
-    </div>
+    <NavBarDiv primary={false}>{timeEntries.length} Time Entries</NavBarDiv>
   );
 };
 
