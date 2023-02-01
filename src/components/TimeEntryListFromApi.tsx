be09b6d483e403ea82a5db463ba4e738373c83a0
Hans-Christian Otto <c.otto@suora.com>
2023-02-01 16:29:22 +0100

    show zod example

--- src/components/TimeEntryListFromApi.tsx
+++ src/components/TimeEntryListFromApi.tsx
@@ -1,6 +1,23 @@
 import TimeEntryList from "./TimeEntryList";
 import React, { useEffect, useState } from "react";
 import TimeEntry from "../domain/TimeEntry";
+import { z } from "zod";
+
+// type TimeEntriesResponse = {
+//   id: string;
+//   comment: string;
+//   start: string;
+//   end: string;
+// }[];
+
+const TimeEntryBackendSchema = z.object({
+  id: z.string(),
+  comment: z.string(),
+  start: z.string(),
+  end: z.string(),
+});
+
+const TimeEntriesBackendResponseSchema = z.array(TimeEntryBackendSchema);
 
 const TimeEntryListFromApi: React.FunctionComponent = () => {
   const [timeEntries, setTimeEntries] = useState<TimeEntry[]>();
@@ -8,8 +25,11 @@ const TimeEntryListFromApi: React.FunctionComponent = () => {
   useEffect(() => {
     fetch("http://localhost:3001/timeEntries")
       .then((response) => response.json())
-      .then((responseData) => {
-        const convertedTimeEntries = responseData.map((timeEntry: any) => {
+      .then((responseData: unknown) => {
+        const backendTimeEntries =
+          TimeEntriesBackendResponseSchema.parse(responseData);
+
+        const convertedTimeEntries = backendTimeEntries.map((timeEntry) => {
           return {
             ...timeEntry,
             start: new Date(timeEntry.start),
